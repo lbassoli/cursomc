e429075c72dd16468f3b4b395152801300e09899
Lucas Bassoli <lfiorin.b@gmail.com>
2018-07-27 00:31:15 -0300

    Implementacao autorizacao

--- src/main/java/com/lucas/cursomc/config/SecurityConfig.java
+++ src/main/java/com/lucas/cursomc/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.lucas.cursomc.security.JWTAuthenticationFilter;
+import com.lucas.cursomc.security.JWTAuthorizationFilter;
 import com.lucas.cursomc.security.JWTUtil;
 
 @Configuration
@@ -50,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().
 			antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 	}
