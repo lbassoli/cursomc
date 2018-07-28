55a16fe8c1cd9c540af136bbd4dc6ae5bd1ef97c
Lucas Bassoli <lfiorin.b@gmail.com>
2018-07-27 23:05:54 -0300

    Listando todas categorias com DTO

---
+++ src/main/java/com/lucas/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,37 @@
+package com.lucas.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.lucas.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+	
+	public CategoriaDTO() {
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
--- src/main/java/com/lucas/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucas/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.lucas.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucas.cursomc.domain.Categoria;
+import com.lucas.cursomc.dto.CategoriaDTO;
 import com.lucas.cursomc.resources.services.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -52,5 +55,11 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() throws ObjectNotFoundException {
+		List<Categoria> lista = service.findAll();
+		return ResponseEntity.ok().body(lista.stream().map(a -> new CategoriaDTO(a)).collect(Collectors.toList()));
 	}
 }
